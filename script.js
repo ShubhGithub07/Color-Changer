f45c1371ec34e1a005c6ac659f4eed1b2435501e
Shubham Yadre <127323514+ShubhGithub07@users.noreply.github.com>
2024-01-29 17:06:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function colorChange(color) {
+    switch(color) {
+          case "red":
+              document.getElementById("color_box").style.backgroundColor = "red";
+              break;
+          case "orange":
+              document.getElementById("color_box").style.backgroundColor = "orange";
+              break;
+          case "yellow":
+              document.getElementById("color_box").style.backgroundColor = "yellow";
+              break;
+          case "green":
+              document.getElementById("color_box").style.backgroundColor = "green";
+              break;
+          case "blue":
+              document.getElementById("color_box").style.backgroundColor = "blue";
+              break;
+          case "indigo":
+              document.getElementById("color_box").style.backgroundColor = "indigo";
+              break;
+          case "violet":
+              document.getElementById("color_box").style.backgroundColor = "violet";
+              break;		
+      }
+  }
